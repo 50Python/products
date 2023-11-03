116c194708ab98bcac90968d0d1e560bd92fe7bc
50Python <C931004@mail4u.tun.edu.tw>
2023-11-03 15:52:19 +0800

    add two-dimensional declare

---
+++ products.py
@@ -0,0 +1,18 @@
+
+products = []
+while True:
+    name = input('請輸入商品名稱: ')
+    if name == 'q':
+    	break
+    price = input('請輸入商品價格: ')
+    #p = []
+    #p.append(name)
+    #p.append(price)
+    #products.append(p)
+    #p =[name, proce]
+    products.append([name, price])
+
+print('目前商品有: ', products[0][0], products[0][1])
+print('目前商品有: ', products[1][0], products[1][1])
+
+
