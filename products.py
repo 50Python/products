73df15677af1e24c038c92546f6428d13db36b32
50Python <C931004@mail4u.tun.edu.tw>
2023-11-03 16:47:12 +0800

    add write into CSV file feature

--- products.py
+++ products.py
@@ -9,13 +9,16 @@ while True:
     #p.append(name)
     #p.append(price)
     #products.append(p)
-    #p =[name, proce]
+    #p =[name, price]
     products.append([name, price])
 
 print('目前商品有: ', products[0][0], products[0][1])
 print('目前商品有: ', products[1][0], products[1][1])
 print('目前商品有: ', products)
 
-for p in products:
-	print(p[0], '的價格是', p[1])
+#for p in products:
+	#print(p[0], '的價格是', p[1])
 
+with open ('product_list.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1]+ '\n')
