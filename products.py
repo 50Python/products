f1b6085bfa6e462bc5a58e1fb25bd0f05bf304f3
50Python <C931004@mail4u.tun.edu.tw>
2023-11-06 18:30:19 +0800

    add file check system

--- products.py
+++ products.py
@@ -1,14 +1,21 @@
+import os
+
+
 #讀取檔案
 
 products = []
 
-with open('product_list.csv', 'r', encoding = 'utf-8') as f:
-    for line in f:
-        if '商品,價格' in line: # 如果商品、價格出現在讀取的字串內
-            continue # 忽略這一次、跳到下一回再開始
-        name, price = line.strip().split(',') #strip()清楚\n , line.spilt 遇到逗點就切割
-        products.append([name, price])
-print(products)
+if os.path.isfile('product_list.csv'):
+    print('資料系統正確')
+    with open('product_list.csv', 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line: # 如果商品、價格出現在讀取的字串內
+                continue # 忽略這一次、跳到下一回再開始
+            name, price = line.strip().split(',') #strip()清楚\n , line.spilt 遇到逗點就切割
+            products.append([name, price])
+    print(products)
+else:
+    print('無存檔資料')
 
 # 使用者輸入
 while True:
